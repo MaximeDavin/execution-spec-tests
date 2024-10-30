9f3e7365bc2a879c5c55228dfc1cc33157efd542
spencer-tb <spencer@spencertaylorbrown.uk>
2024-10-30 18:09:52 +0700

    chore(ruff): changes to `src/ethereum_test_base_types`.

--- src/ethereum_test_base_types/json.py
+++ src/ethereum_test_base_types/json.py
@@ -1,6 +1,4 @@
-"""
-JSON encoding and decoding for Ethereum types.
-"""
+"""JSON encoding and decoding for Ethereum types."""
 
 from typing import Any, AnyStr, List
 
@@ -8,14 +6,12 @@ from pydantic import BaseModel, RootModel
 
 
 def to_json(
-    input: BaseModel | RootModel | AnyStr | List[BaseModel | RootModel | AnyStr],
+    input_model: BaseModel | RootModel | AnyStr | List[BaseModel | RootModel | AnyStr],
 ) -> Any:
-    """
-    Converts a model to its json data representation.
-    """
-    if isinstance(input, list):
-        return [to_json(item) for item in input]
-    elif isinstance(input, (BaseModel, RootModel)):
-        return input.model_dump(mode="json", by_alias=True, exclude_none=True)
+    """Convert a model to its json data representation."""
+    if isinstance(input_model, list):
+        return [to_json(item) for item in input_model]
+    elif isinstance(input_model, (BaseModel, RootModel)):
+        return input_model.model_dump(mode="json", by_alias=True, exclude_none=True)
     else:
-        return str(input)
+        return str(input_model)
